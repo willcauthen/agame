e8965e95f4e8fad4e9d9574746039caff89e0b6e
Will Cauthen <willcauthen50@gmail.com>
2016-05-21 16:47:55 -0700

    component folder structure is structured

---
+++ src/components/Log.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="log">
+		<h2>{{ msg }}</h2>
+    <ul>
+      <li></li>
+    </ul>
+  </div>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        msg: 'here lie the logs'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  div.log {
+    background: blue;
+  }
+  
+</style>
