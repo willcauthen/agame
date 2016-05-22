06ee84c6db758e824457dfa06c51565bc0737008
Will Cauthen <willcauthen50@gmail.com>
2016-05-22 13:07:07 -0700

    still testing

---
+++ src/components/actions/action.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+  	<button class="stat1btn" @click='increment'>{{ name }}</button>
+  </div>
+</template>
+
+<script>
+  import { incrementCounter } from '../../vuex/actions'
+  export default {
+    vuex: {
+      actions: {
+        increment: incrementCounter
+      }
+    },
+    data () {
+      return {
+        name: 'simple stat'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
