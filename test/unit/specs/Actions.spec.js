d5a52aff9497f17558daba1bc550e9ed8362a585
Will Cauthen <willcauthen50@gmail.com>
2016-05-22 13:06:52 -0700

    testing

--- test/unit/specs/Actions.spec.js
+++ test/unit/specs/Actions.spec.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import Actions from 'src/components/actions/Actions'
+import store from 'src/vuex/store'
+Vue.use(Vuex)
 
 describe('Actions.vue', () => {
   it('It should render its header', () => {
     const vm = new Vue({
       template: '<div><actions></actions></div>',
-      components: { Actions }
+      components: { Actions },
+      store: store
     }).$mount()
     expect(vm.$el.querySelector('#actions h2').textContent).to.contain('Action Panel')
   })
