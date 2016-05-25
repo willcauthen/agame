ea0c3e6d866465d2339af1d11f82c02f0b3b6566
Will Cauthen <willcauthen50@gmail.com>
2016-05-25 15:50:20 -0700

    research button implemented

---
+++ src/components/actions/research.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <button id="research">Fervently research</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        msg: 'research thing is wired and primed'
+      }
+    }
+  }
+</script>
