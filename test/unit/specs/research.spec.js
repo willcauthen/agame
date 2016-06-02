b415b91766a456fde343e0a24fbf6f7ce6a608f5
Will Cauthen <willcauthen50@gmail.com>
2016-06-02 11:58:22 -0700

    test also verifies that a event item was appended to events

--- test/unit/specs/research.spec.js
+++ test/unit/specs/research.spec.js
@@ -4,7 +4,7 @@ import research from 'src/components/actions/research'
 import store from 'src/vuex/store'
 
 describe('research.vue', () => {
-  it('research should contain 3 buttons, each with a class name to their stat', () => {
+  it('research should contain 3 buttons, each with a class name to their stat, each should increment that stat, and each should append a message to events', () => {
     const vm = new Vue({
       template: '<div><research></research></div>',
       components: { Stat, research },
@@ -13,18 +13,21 @@ describe('research.vue', () => {
     expect(vm.$el.querySelector('button.intelligence')).to.be.ok
     expect(vm.$el.querySelector('button.strength')).to.be.ok
     expect(vm.$el.querySelector('button.guile')).to.be.ok
-    // each button should increment it's stat
-    // intelligence, should always be spelled with two 'l's
+    // each button should increment it's stat and append to log events
+    // intelligence   (should always be spelled with two 'l's)
     expect(store.state.stats.intelligence.count).to.equal(0)
     vm.$el.querySelector('button.intelligence').click()
     expect(store.state.stats.intelligence.count).to.not.equal(0)
+    expect(store.state.events.pop().thing).to.equal('read some books')
     // strength
     expect(store.state.stats.strength.count).to.equal(0)
     vm.$el.querySelector('button.strength').click()
     expect(store.state.stats.strength.count).to.not.equal(0)
+    expect(store.state.events.pop().thing).to.equal('do some push-ups')
     // guile
     expect(store.state.stats.guile.count).to.equal(0)
     vm.$el.querySelector('button.guile').click()
     expect(store.state.stats.guile.count).to.not.equal(0)
+    expect(store.state.events.pop().thing).to.equal('snatch some purses')
   })
 })
